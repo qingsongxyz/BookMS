33c292835fa5ed15e579916421aca3b94516d0f1
qingsongxyz <201931119020054@stu.hubu.edu.cn>
2022-03-09 20:33:17 +0800

    master/test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("test");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("master/test");
     }
 }
