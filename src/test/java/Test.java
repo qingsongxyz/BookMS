b5793ae032995d353d45921ce23e8d1d88cf5332
qingsongxyz <201931119020054@stu.hubu.edu.cn>
2022-03-09 20:46:48 +0800

    完成

--- src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
-        System.out.println("test3");
-        System.out.println("master/test");
-        System.out.println("branch1/test");
-    }
-}
